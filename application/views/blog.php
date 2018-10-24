279e6d56a05073cffa795da66d319828f51bc73a
John Doe <anilkushwaha.ind@gmail.com>
2018-10-24 16:39:42 +0530

    portfolio start

--- application/views/blog.php
+++ application/views/blog.php
@@ -2,43 +2,45 @@
 
 
 
-	<!-- blog container start-->
+  <!-- blog container start-->
   <div class="col-md-8 blog">
-  	<?php for ($i=0; $i < 10; $i++) { ?>
- 	<div class="clearfix">
-	<img class="img thumbnail" src="assets/paris.jpg" alt="demo" width="170" height="170">
-	<h2><strong>Lorem ipsum</strong></h2>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum...</p>
-	</div>	
-	<?php } ?>
+    <?php for ($i=0; $i < 10; $i++) { ?>
+  <div class="clearfix">
+  <img class="img thumbnail" src="assets/paris.jpg" alt="demo" width="170" height="170">
+  <h2><strong>Lorem ipsum</strong></h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum...</p>
+  </div>  
+  <?php } ?>
   </div>
   <!-- blog container end -->
 
 
 <!-- rightbar start no-1-->
-  <div class="col-md-4 blogsidebar">
-  	<h3><strong style="color: #4CAF50;">Popular Post</strong></h3>
-  	  	<?php for ($i=0; $i < 5; $i++) { ?>
-   	<div class="clearfix">
-	<img class="img thumbnail" src="assets/paris.jpg" alt="demo" width="170" height="170">
-	<h4><strong>Lorem ipsum</strong></h4>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-	</div>
-		<?php } ?>
+<div class="col-md-4">
+  <div class="blogsidebar">
+    <h3><strong style="color: #4CAF50;">Popular Post</strong></h3>
+        <?php for ($i=0; $i < 5; $i++) { ?>
+    <div class="clearfix">
+  <img class="img thumbnail" src="assets/paris.jpg" alt="demo" width="170" height="170">
+  <h4><strong>Lorem ipsum</strong></h4>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+  </div>
+    <?php } ?>
   </div>  
   <!-- rightbar end -->
 
   <!-- rightbar start no-2-->
-  <div class="col-md-4 blogsidebar">
-  	<h3><strong style="color: #e8016c;">Latest Post</strong></h3>
-  	  	<?php for ($i=0; $i < 5; $i++) { ?>
-   	<div class="clearfix">
-	<img class="img thumbnail" src="assets/paris.jpg" alt="demo" width="170" height="170">
-	<h4><strong>Lorem ipsum</strong></h4>
-	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-	</div>
-		<?php } ?>
+  <div class=" blogsidebar">
+    <h3><strong style="color: #e8016c;">Latest Post</strong></h3>
+        <?php for ($i=0; $i < 5; $i++) { ?>
+    <div class="clearfix">
+  <img class="img thumbnail" src="assets/paris.jpg" alt="demo" width="170" height="170">
+  <h4><strong>Lorem ipsum</strong></h4>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+  </div>
+    <?php } ?>
   </div>  
   <!-- rightbar end -->
+</div>
 </div>
 
--- application/views/index.php
+++ application/views/index.php
@@ -1,19 +1,46 @@
 <div class="content container">
-  <div class="col-md-8">
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-  </div>
-  <div class="col-md-4">
-     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+
+  <!-- blog container start-->
+  <div class="col-md-8 blog">
+    <?php for ($i=0; $i < 10; $i++) { ?>
+  <div class="clearfix">
+  <img class="img thumbnail" src="assets/paris.jpg" alt="demo" width="170" height="170">
+  <h2><strong>Lorem ipsum</strong></h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum...</p>
   </div>  
+  <?php } ?>
+  </div>
+  <!-- blog container end -->
+
+
+<!-- rightbar start no-1-->
+<div class="col-md-4">
+  <div class="blogsidebar">
+    <h3><strong style="color: #4CAF50;">Popular Post</strong></h3>
+        <?php for ($i=0; $i < 5; $i++) { ?>
+    <div class="clearfix">
+  <img class="img thumbnail" src="assets/paris.jpg" alt="demo" width="170" height="170">
+  <h4><strong>Lorem ipsum</strong></h4>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+  </div>
+    <?php } ?>
+  </div>  
+  <!-- rightbar end -->
+
+  <!-- rightbar start no-2-->
+  <div class=" blogsidebar">
+    <h3><strong style="color: #e8016c;">Latest Post</strong></h3>
+        <?php for ($i=0; $i < 5; $i++) { ?>
+    <div class="clearfix">
+  <img class="img thumbnail" src="assets/paris.jpg" alt="demo" width="170" height="170">
+  <h4><strong>Lorem ipsum</strong></h4>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+  </div>
+    <?php } ?>
+  </div>  
+  <!-- rightbar end -->
+</div>
 </div>
 
