747d8b720e3722feda5559ca27b1e064e8286379
John Doe <anilkushwaha.ind@gmail.com>
2018-10-25 17:22:55 +0530

    sony now

---
+++ application/views/about.php
@@ -0,0 +1,27 @@
+<div class="content container">
+  <!-- blog container start-->
+  <div class="col-md-8 contact">
+    author about
+  </div>
+  <!-- blog container end -->
+
+
+<!-- rightbar start no-1-->
+<div class="col-md-4">
+  <div class="card">
+  <img src="assets/paris.jpg" alt="John" style="width:100%">
+  <h1>John Doe</h1>
+  <p class="title">CEO & Founder, Example</p>
+  <p>Harvard University</p>
+  <div style="margin: 24px 0;">
+    <a href="#"><i class="fa fa-dribbble"></i></a> 
+    <a href="#"><i class="fa fa-twitter"></i></a>  
+    <a href="#"><i class="fa fa-linkedin"></i></a>  
+    <a href="#"><i class="fa fa-facebook"></i></a> 
+ </div>
+ <p><button>Contact</button></p>
+</div>
+</div>
+<!-- rightbar start no-1-->
+</div>
+
